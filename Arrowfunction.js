7c27f6bcdefb4544a9069acd597f244127b74328
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2022-12-08 23:19:00 +0530

    Callstackoverflow

---
+++ Arrowfunction.js
@@ -0,0 +1,26 @@
+/*Function expression:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+//Arrow fnction
+let sum = (a, b) => a + b;
+console.log(sum(9, 7)); 
+
+//optional parantheses for single parameter
+var n  = x =>{
+    console.log(x)
+}
+n(30);
+
+//Arrow function with parameter
+let add = (a,b,c) => {  
+    console.log(a + b + c );  
+}  
+add(100,200,300);  
+
+let odds  = evens.map(v => v + 1)
+let pairs = evens.map(v => ({ even: v, odd: v + 1 }))
+let nums  = evens.map((v, i) => v + i)
---
+++ callstack.js
@@ -0,0 +1,27 @@
+
+function n1()
+{
+console.log("learning html")
+n2()
+}
+function n2()
+{
+console.log("learning css")
+n3()
+}
+function n3()
+{
+console.log("learning Js")
+n4()
+}
+function n4()
+{
+console.log("learning Angular")
+n5()
+}
+function n5()
+{
+console.log("learning React")
+throw new error("error occured")
+}
+n1()
