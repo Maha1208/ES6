a0b871884b17207e50eefe81affee4f760e4966c
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2022-12-11 20:28:17 +0530

    EnhancedRegexp

--- EnhancedRE.js
+++ EnhancedRE.js
@@ -1,1 +1,6 @@
 //Regular Expression Sticky Matching
+const str1 = 'table football';
+const regex1 = new RegExp('foo', 'y');
+regex1.lastIndex = 6;
+console.log(regex1.sticky);
+console.log(regex1.test(str1));
