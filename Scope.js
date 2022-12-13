946f515c46a38ebe181017fc05ee408200ca6e28
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2022-12-13 11:17:46 +0530

    Scope

--- Scope.js
+++ Scope.js
@@ -1,14 +1,38 @@
 // Global scope
 let zooKeeper = "I'm global!"
-function zooAnimalEnivornment() {
+function zooAnimalEnivornment()
+{
   // Local scope
   let zooAnimal = "I'm a local!"
 }
-console.log(zooKeeper) // I'm global!
+console.log(zooKeeper)
 console.log(zooAnimal) // Uncaught ReferenceError: zooAnimal is not defined
 
 //defining
-for (let i = 0; i < 3; i++) {
-    console.log(i) // logs: 0 1 2
-  }
+for (let i = 0; i < 3; i++) 
+{
+    console.log(i) 
+}
   console.log(i) //  i is not defined
+
+  //without hoisting
+  let a=0; let b=0;
+  for (let i = 0; i < a.length; i++) {
+    let x = a[i]
+
+}
+for (let i = 0; i < b.length; i++) {
+    let y = b[i]
+
+}
+
+let callbacks = []
+for (let i = 0; i <= 2; i++) {
+    callbacks[i] = function () 
+    { 
+      return i * 2 
+    }
+}
+console.log(callbacks[0]() === 0)
+console.log(callbacks[1]() === 2)
+console.log(callbacks[2]() === 4)
