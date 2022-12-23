7f098a87bf1965986a0552e6048f5f69221690b1
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2022-12-23 13:23:19 +0530

    commit

--- Stringliteral.js
+++ Stringliteral.js
@@ -6,13 +6,7 @@ want to buy ${card.amount} ${card.product} for
 a total of ${card.amount * card.unitprice} bucks?`
 console.log(message);
 
-
 //Raw String access
 const link = String.raw`http://es6-features.org/#RawStringAccess`
 console.log(`please copy the given link ${link} and paste it in chrome`)
-
-//custom interpolation
-get`http://example.com/foo?bar=${bar + baz}&quux=${quux}`
-
-console.log('\ud83d\ud65d\ud83d\udc71\u2754');
 
