2fa041101353f142c6ff1c9c2d22a3fef7929657
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2022-12-23 13:20:45 +0530

    commit

--- Scope.js
+++ Scope.js
@@ -15,24 +15,3 @@ for (let i = 0; i < 3; i++)
 }
   console.log(i) //  i is not defined
 
-  //without hoisting
-  let a=0; let b=0;
-  for (let i = 0; i < a.length; i++) {
-    let x = a[i]
-
-}
-for (let i = 0; i < b.length; i++) {
-    let y = b[i]
-
-}
-
-let callbacks = []
-for (let i = 0; i <= 2; i++) {
-    callbacks[i] = function () 
-    { 
-      return i * 2 
-    }
-}
-console.log(callbacks[0]() === 0)
-console.log(callbacks[1]() === 2)
-console.log(callbacks[2]() === 4)
